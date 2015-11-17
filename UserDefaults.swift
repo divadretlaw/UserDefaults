67ae67c91833e22fdf17ae6c121d8b2befe2cc49
David Walter <e1426861@student.tuwien.ac.at>
2015-11-17 22:32:51 +0100

    Fix for clear

--- UserDefaults.swift
+++ UserDefaults.swift
@@ -23,35 +23,35 @@
 //
 
 #if os(iOS)
-    
+
 import UIKit
-    
+
 #elseif os(OSX)
-    
+
 import AppKit
-    
+
 #endif
 
 class UserDefaults {
-    
+
     class func set(array: NSArray?, key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(array, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     class func set(data: NSData?, key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(data, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     class func set(dictionary: NSDictionary?, key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(dictionary, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     /**
      * Also accepts an integer, float, double or Boolean value
      */
@@ -60,96 +60,96 @@ class UserDefaults {
         userDefaults.setObject(number, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     class func set(string: String?, key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(string, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     class func set(url: NSURL?, key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(url, forKey: key)
         userDefaults.synchronize()
     }
-    
+
     /**
      * Returns the array associated with the specified key
      */
     class func array(key: String) -> NSArray? {
         return NSUserDefaults.standardUserDefaults().arrayForKey(key)
     }
-    
+
     /**
      * Returns the data object associated with the specified key
      */
     class func data(key: String) -> NSData? {
         return NSUserDefaults.standardUserDefaults().dataForKey(key)
     }
-    
+
     //NSNumber types
-    
+
     /**
      * Returns the Boolean value associated with the specified key
      */
     class func bool(key: String) -> Bool {
         return NSUserDefaults.standardUserDefaults().boolForKey(key)
     }
-    
+
     /**
      * Returns the double value associated with the specified key
      */
     class func double(key: String) -> Double {
         return NSUserDefaults.standardUserDefaults().doubleForKey(key)
     }
-    
+
     /**
      * Returns the float value associated with the specified key
      */
     class func float(key: String) -> Float {
         return NSUserDefaults.standardUserDefaults().floatForKey(key)
     }
-    
+
     /**
      * Returns the integer value associated with the specified key
      */
     class func integer(key: String) -> Int {
         return NSUserDefaults.standardUserDefaults().integerForKey(key)
     }
-    
+
     //NSNumber types
-    
+
     /**
      * Returns the dictionary object associated with the specified key
      */
     class func dictionary(key: String) -> NSDictionary? {
         return NSUserDefaults.standardUserDefaults().dictionaryForKey(key)
     }
-    
+
     /**
      * Returns the string associated with the specified key
      */
     class func string(key: String) -> String? {
         return NSUserDefaults.standardUserDefaults().stringForKey(key)
     }
-    
+
     /**
      * Returns the NSURL instance associated with the specified key
      */
     class func url(key: String) -> NSURL? {
         return NSUserDefaults.standardUserDefaults().URLForKey(key)
     }
-    
-    
+
+
     /**
      * Deletes the stored value associated with the specified key
      */
     class func clear(key: String) {
         let userDefaults = NSUserDefaults.standardUserDefaults()
-        userDefaults.setObject(nil, forKey: key)
+        userDefaults.removeObjectForKey(key)
         userDefaults.synchronize()
     }
-    
+
     /**
      * Deletes every stored value in the UserDefaults
      */
@@ -158,5 +158,5 @@ class UserDefaults {
             NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain)
         }
     }
-    
+
 }
